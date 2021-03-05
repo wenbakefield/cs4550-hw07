90f48c477c66562993921b2ec4cc03f205232d12
wenbakefield <wenbakefield@gmail.com>
2021-03-05 06:41:36 -0500

    fixed deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export MIX_ENV=prod
-export PORT=4270
+export PORT=4271
 export SECRET_KEY_BASE=insecure
+export DATABASE_URL=ecto://"events":"oud0aeCh1ahz"@localhost/events
 
 echo "Building..."
 
@@ -12,7 +13,7 @@ mix compile
 # Taken from CS4550 lecture notes
 # Author: Nat Tuck
 # Attribution: https://github.com/NatTuck/scratch-2021-01/blob/master/4550/0212/hangman/deploy.sh
-CFGD=$(readlink -f ~/.config/bulls)
+CFGD=$(readlink -f ~/.config/events)
 
 if [ ! -d "$CFGD" ]; then
     mkdir -p "$CFGD"
@@ -31,9 +32,6 @@ mix phx.digest
 
 echo "Generating release..."
 mix release
-
-#echo "Stopping old copy of app, if any..."
-#_build/prod/rel/bulls/bin/bulls stop || true
 
 echo "Starting app..."
 
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,13 +9,3 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-
-alias Events.Repo
-alias Events.Users.User
-alias Events.Posts.Post
-
-alice = Repo.insert!(%User{name: "Alice", email: "alice@gmail.com"})
-bob = Repo.insert!(%User{name: "Bob", email: "bob@gmail.com"})
-
-Repo.insert!(%Post{user_id: alice.id, title: "Alice's Party", date: ~N[2000-01-01 23:00:07], description: "Please come."})
-Repo.insert!(%Post{user_id: bob.id, title: "Bob's BBQ", date: ~N[2000-01-01 23:00:07], description: "I cook a mean steak."})
